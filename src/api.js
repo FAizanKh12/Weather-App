591c07b7f091cb7078a478cb28b3878fe08c9c19
Faizan Khan <140182277+FAizanKh12@users.noreply.github.com>
2024-12-27 10:43:56 +0530

    Add files via upload

---
+++ src/api.js
@@ -0,0 +1,13 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'x-rapidapi-key': '3ea4cdb738msh75d7f47feb07145p1c7de3jsn1816dd585891',
+		'x-rapidapi-host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "2de2a075fd49cb87d802fe6788b5baa6";
+
